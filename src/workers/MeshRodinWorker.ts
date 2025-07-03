fec6f3aec6c53bfce668759e7ff0de791f271aec
fnrafa <fikrinoor790@gmail.com>
2025-07-04 02:55:10 +0700

    fix: path position

--- src/workers/MeshRodinWorker.ts
+++ src/workers/MeshRodinWorker.ts
@@ -50,10 +50,9 @@ class MeshRodinWorker {
         localGlbPath: string,
         localOutPath: string
     ): Promise<void> {
-        const projectRoot = path.resolve(__dirname, "../../");
+        const projectRoot = path.resolve(__dirname, "../../../");
 
         const scriptPath = path.join(projectRoot, "render_thumb.py");
-
         const absGlb = path.join(projectRoot, localGlbPath);
         const absOut = path.join(projectRoot, localOutPath);
 
@@ -89,6 +88,7 @@ class MeshRodinWorker {
             });
         });
     }
+
 
     private async processTask(): Promise<void> {
         WebSocket.sendMessage(this.taskId, "processing", "Worker started processing Rodin model.");
